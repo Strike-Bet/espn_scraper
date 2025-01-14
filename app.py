eccfd3da3d4cb0113e3553e31e7ee281e395c92b
EC2 Default User <ec2-user@ip-172-31-85-37.ec2.internal>
2025-01-14 20:26:13 +0000

    updated ports and docker setup for deployemnt

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def test_processors():
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=8000)
